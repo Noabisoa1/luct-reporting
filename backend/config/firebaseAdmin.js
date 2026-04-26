f5ea110e4cd6d28582ee15784b11d3a5981c5463
Noabisa <noabisoa@gmail.com>
2026-04-26 23:25:43 +0200

    Move Firebase service account to environment variables

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,5 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+require("dotenv").config();
+
+const serviceAccount = {
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  privateKey: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
